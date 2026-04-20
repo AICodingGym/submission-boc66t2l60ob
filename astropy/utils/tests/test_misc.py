a367540edb7f07c40ea4cac24324eb899e43bc37
x451711881 <theima1994@outlook.com>
2026-04-20 14:31:40 -0700

    Solution attempt for astropy_astropy-7166

--- astropy/utils/tests/test_misc.py
+++ astropy/utils/tests/test_misc.py
@@ -89,6 +89,21 @@ def test_inherit_docstrings():
         assert Subclass.__call__.__doc__ == "FOO"
 
 
+def test_inherit_docstrings_for_properties():
+    class Base(metaclass=misc.InheritDocstrings):
+        @property
+        def foo(self):
+            "FOO"
+            return 1
+
+    class Subclass(Base):
+        @property
+        def foo(self):
+            return 2
+
+    assert Subclass.foo.__doc__ == "FOO"
+
+
 def test_set_locale():
     # First, test if the required locales are available
     current = locale.setlocale(locale.LC_ALL)
